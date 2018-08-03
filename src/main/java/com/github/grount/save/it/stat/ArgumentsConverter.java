03445fcf64130f85a3d9f88194963a89051f3207
Daniel Grount <danielgr@mailstud.hit.ac.il>
2018-08-04 01:33:53 +0300

    Add utility that is responsible for converting the command line arguments

---
+++ src/main/java/com/github/grount/save/it/stat/ArgumentsConverter.java
@@ -0,0 +1,98 @@
+package com.github.grount.save.it.stat;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ArgumentsConverter {
+    private static final String[] FULL_ARGUMENTS = {"title", "content"};
+    private static final String VALID_ARGUMENT_REGEX =
+            "^(\\-|\\-\\-)[a-z]*=\".*\""; // The regex is in the form of --".*" or --".*"
+
+    private static Set<String> validArgumentsSet;
+    private static Map<String, String> shortArgumentMap;
+
+    static {
+        validArgumentsSet = createArgumentsSet();
+        shortArgumentMap = createShortArgumentMap();
+    }
+
+    private ArgumentsConverter() {
+    }
+
+    public static Map<String,String> convert(String[] arguments) {
+        checkFormValidity(arguments);
+        Map<String, String> splittedArguments = splitArguments(arguments);
+        checkLeftSideValidity(splittedArguments);
+        return convertShortArgumentsToFull(splittedArguments);
+    }
+
+    private static Map<String,String> convertShortArgumentsToFull(Map<String, String> arguments) {
+        HashMap<String, String> newArgumentMap = new HashMap<>();
+
+        arguments.forEach((key, value) -> {
+            if (shortArgumentMap.containsKey(key)) {
+                newArgumentMap.put(shortArgumentMap.get(key), value);
+            } else {
+                newArgumentMap.put(key, value);
+            }
+        });
+
+        return newArgumentMap;
+    }
+
+    private static Map<String, String> createShortArgumentMap() {
+        HashMap<String, String> shortArgumentMap = new HashMap<>();
+
+        validArgumentsSet.forEach(argument -> shortArgumentMap.put(getArgumentFirstLetter(argument), argument));
+        return shortArgumentMap;
+    }
+
+    private static Set<String> createArgumentsSet() {
+        Set<String> arguments = new HashSet<>();
+
+        for (String argument : FULL_ARGUMENTS) {
+            arguments.add(argument);
+            arguments.add(getArgumentFirstLetter(argument));
+        }
+
+        return arguments;
+    }
+
+    private static String getArgumentFirstLetter(String argument) {
+        return Character.toString(argument.charAt(0));
+    }
+
+    public static void checkFormValidity(String[] arguments) {
+        Pattern pattern = Pattern.compile(VALID_ARGUMENT_REGEX);
+        for (String argument : arguments) {
+            Matcher matcher = pattern.matcher(argument);
+            if (!matcher.find())
+                throw new IllegalArgumentException("Each argument should start with -- or -");
+        }
+    }
+
+    private static void checkLeftSideValidity(Map<String, String> arguments) {
+        arguments.forEach((key,value) -> {
+            if (!validArgumentsSet.contains(key))
+                throw new IllegalArgumentException(String.format("The argument: %s is invalid", key));
+        });
+    }
+
+    private static Map<String, String> splitArguments(String[] arguments) {
+        HashMap<String, String> splittedArguments = new HashMap<>();
+
+        for (String argument : arguments) {
+            String[] values = argument.split("=", 2);
+            String leftValue = trimStartingMinus(values[0]);
+            String rightValue = values[1].substring(1, values[1].length() - 1);
+            splittedArguments.put(leftValue, rightValue);
+        }
+
+        return splittedArguments;
+    }
+
+    private static String trimStartingMinus(String value) {
+        return value.replaceFirst("^\\-+", "");
+    }
+}
---
+++ src/test/java/com/github/grount/save/it/stat/ArgumentsConverterTest.java
@@ -0,0 +1,78 @@
+package com.github.grount.save.it.stat;
+
+import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.github.grount.save.it.stat.ArgumentsConverter.checkFormValidity;
+import static com.github.grount.save.it.stat.ArgumentsConverter.convert;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgumentsConverterTest {
+    private String[] validFullArguments = {"--title=\"title\"", "--content=\"content\""};
+    private String[] invalidFullArguments = {"--title=\"title\"", "--contant=\"content\""};
+    private String[] validShortArguments = {"-t=\"title\"", "-c=\"content\""};
+    private String[] invalidShortArguments = {"-k=\"t\"", "-c=\"content\""};
+    private String[] validMixedArguments = {"-t=\"title\"", "--content=\"content\""};
+    private String[] validForm = {"-k=\"value 342\"", "--y=\"val\""};
+    private String[] invalidForm = {"title=\"title\"", "--content=\"content\"", "-test=\"t\""};
+
+    @Test
+    @DisplayName("Valid full name arguments should not throw exception")
+    void convert_validFullArguments_ExceptionNotThrown() {
+        assertDoesNotThrow(() -> convert(validFullArguments));
+        assertDoesNotThrow(() -> convert(validShortArguments));
+        assertDoesNotThrow(() -> convert(validMixedArguments));
+    }
+
+    @Test
+    @DisplayName("Invalid full name arguments should throw exception")
+    void convert_invalidFullArguments_ExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> convert(invalidFullArguments));
+        assertThrows(IllegalArgumentException.class, () -> convert(invalidShortArguments));
+    }
+
+    @Test
+    @DisplayName("Arguments with valid form, starts with -- or - should not throw exception")
+    void checkFormValidity_validForm_ExceptionNotThrown() {
+        assertDoesNotThrow(() -> checkFormValidity(validForm));
+    }
+
+    @Test
+    @DisplayName("Arguments with invalid form, that doesn't start with -- or - should throw exception")
+    void checkFormValidity_invalidForm_ExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> checkFormValidity(invalidForm));
+    }
+
+
+    @Nested
+    @DisplayName("Test convert helper class")
+    class testConvertHelper {
+        private Map<String,String> argumentsMap;
+        @BeforeEach
+        void initAll() {
+            argumentsMap = new HashMap<>();
+            argumentsMap.put("content", "content");
+            argumentsMap.put("title", "title");
+        }
+
+        @Test
+        @DisplayName("Convert full arguments should return a valid map")
+        void convert_givenFullArguments_returnValidMap() {
+            assertTrue(convert(validFullArguments).equals(argumentsMap));
+        }
+
+        @Test
+        @DisplayName("Convert short arguments should return a valid map")
+        void convert_givenShortArguments_returnValidMap() {
+            assertTrue(convert(validShortArguments).equals(argumentsMap));
+        }
+
+        @Test
+        @DisplayName("Convert mixed arguments should return a valid map")
+        void convert_givenMixedArguments_returnValidMap() {
+            assertTrue(convert(validMixedArguments).equals(argumentsMap));
+        }
+    }
+}
