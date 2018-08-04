b1d4b652fbfea6aeb5d908bcda7fe0bf5903d3c5
Daniel Gront <grount1@gmail.com>
2018-08-05 02:45:50 +0300

    Refactor JsonGenerator and fix compile issue

--- src/main/java/com/github/grount/save/it/stat/JsonGenerator.java
+++ src/main/java/com/github/grount/save/it/stat/JsonGenerator.java
@@ -1,15 +1,14 @@
 package com.github.grount.save.it.stat;
 
-import org.codehaus.jackson.annotate.JsonValue;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 import javax.annotation.Nonnull;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -18,6 +17,8 @@ import java.util.logging.Logger;
 public class JsonGenerator {
     private static final LogManager logManager = LogManager.getLogManager();
     private static final Logger logger = logManager.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private static final String ELEMENTS_JSON = "elements.json";
+    private static final String ELEMENTS_NAME = "elements";
     private static Path path;
     private static Type type;
 
@@ -26,7 +27,7 @@ public class JsonGenerator {
 
     public static void generate(@Nonnull Type type) {
         JsonGenerator.type = type;
-        path = Paths.get("elements.json");
+        path = Paths.get(ELEMENTS_JSON);
 
         if (path.toFile().exists())
             appendTypeToExistingJson();
@@ -39,7 +40,7 @@ public class JsonGenerator {
             JSONObject parent = new JSONObject();
             JSONArray jsonArray = new JSONArray();
             jsonArray.put(objectToWrite);
-            parent.put("elements", jsonArray);
+            parent.put(ELEMENTS_NAME, jsonArray);
             bw.write(parent.toString());
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Cannot save type to json: {0}", e.getMessage());
@@ -52,19 +53,18 @@ public class JsonGenerator {
             String json = new String(Files.readAllBytes(path));
             JSONObject jsonObject = new JSONObject(json);
             appendToJson(jsonObject);
-            createJsonWithType(jsonObject);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Cannot append type to json: {0}", e.getMessage());
         }
     }
 
     private static void appendToJson(JSONObject jsonObject) {
-        JSONArray jsonArray = jsonObject.getJSONArray("elements");
+        JSONArray jsonArray = jsonObject.getJSONArray(ELEMENTS_NAME);
         jsonArray.put(type.getJsonObject());
 
         try (BufferedWriter bw = Files.newBufferedWriter(path)) {
             JSONObject parent = new JSONObject();
-            parent.put("elements", jsonArray);
+            parent.put(ELEMENTS_NAME, jsonArray);
             bw.write(parent.toString());
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Cannot save type to json: {0}", e.getMessage());
