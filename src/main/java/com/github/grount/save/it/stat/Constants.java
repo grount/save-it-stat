3ca833e67be71b40e249c927cdc91237d95ff68f
Daniel Gront <grount1@gmail.com>
2018-08-08 10:05:37 +0300

    Moved default path one folder deeper

--- src/main/java/com/github/grount/save/it/stat/Constants.java
+++ src/main/java/com/github/grount/save/it/stat/Constants.java
@@ -2,5 +2,5 @@ package com.github.grount.save.it.stat;
 
 public class Constants {
     private Constants() {}
-    public static final String DEFAULT_PATH = "src//main//javascript//";
+    public static final String DEFAULT_PATH = "src//main//javascript//src//";
 }
