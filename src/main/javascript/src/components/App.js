47187adb5b2129bed3b0bd6e67924377aaf0e2ef
Daniel Gront <grount1@gmail.com>
2018-08-08 09:00:08 +0300

    Clean up of the react side and add eslintrc file

---
+++ src/main/javascript/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    let data = require('../../../../../elements.json');
+
+  }
+  render() {
+    return (
+      <h1> test</h1>
+    );
+  }
+}
+
+export default App;
