a88ab3da1b5bbbaba46e39a78bbd84baaad71de0
Daniel Gront <grount1@gmail.com>
2018-08-08 10:04:47 +0300

    Working minimal edition with text items only

--- src/main/javascript/src/components/App.js
+++ src/main/javascript/src/components/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import Element from './Element';
+import { ListGroup, Container } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    let data = require('../../../../../elements.json');
-
+    this.state = {
+      json: require('../elements.json')
+    }
   }
+
+  generateElements = () => {
+    const elements = this.state.json.elements.map(element => {
+      return <Element
+        id={element.id}
+        title={element.title}
+        text={element.content}
+      />
+    });
+    return elements;
+  }
+
   render() {
     return (
-      <h1> test</h1>
+      <Container>
+        <ListGroup>
+          {this.generateElements()}
+        </ListGroup>
+      </Container>
     );
   }
 }
